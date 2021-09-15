89157be5421492dae3419526da1a02d2472fae67
usuyn <usuyn000@gmail.com>
2021-09-15 23:54:31 +0900

    move : create colors.js and move darkLightHandler code
    기존 script.js에 작성되어 있던 darkLightHandler와 그에 필요한 객체들을 colors.js에 옮겼습니다.

--- google-home-clone/colors.js
+++ google-home-clone/colors.js
@@ -1,19 +1,3 @@
-const clearBtn = document.querySelector(".clear-btn");
-const searchBar = document.querySelector(".search-bar");
-
-searchBar.addEventListener("keyup", function() {
-    if(searchBar.value && clearBtn.style.visibility != "visible"){
-        clearBtn.style.visibility = "visible"
-    } else if(!searchBar.value) {
-        clearBtn.style.visibility = "hidden"
-    }
-});
-
-clearBtn.addEventListener("click", function() {
-    searchBar.value = "";
-    clearBtn.style.visibility = "hidden";
-});
-
 var Body = {
     setBackgroundColor: function (color) {
         // document.querySelector('body').style.backgroundColor = color;
