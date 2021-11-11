a52b401f5b9be3d0df8d7aff6cafaab543f01d76
usuyn <usuyn000@gmail.com>
2021-11-12 02:19:30 +0900

    Modify(main.js) : change duplicate variable name
    기존 url 변수명을 _url로 변경하고 새로운 변수 url을 추가했습니다.

--- NodeJS-practice/main.js
+++ NodeJS-practice/main.js
@@ -1,18 +1,20 @@
 let http = require('http');
 let fs = require('fs');
+let url = require('url');
+
 let app = http.createServer(function(request,response){
-  let url = request.url;
-  console.log(url);
-  if(url == '/'){
-    url = '/index.html';
+  let _url = request.url;
+  console.log(_url);
+  if(_url == '/'){
+    _url = '/index.html';
   }
-  if(url == '/favicon.ico'){
+  if(_url == '/favicon.ico'){
     response.writeHead(404);
     response.end();
     return;
   }
   response.writeHead(200);
   //console.log(__dirname + url);
-  response.end(fs.readFileSync(__dirname + url));
+  response.end(fs.readFileSync(__dirname + _url));
 });
 app.listen(3000);
