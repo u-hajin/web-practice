52e091664047cff952aa110c5edf51a0d7276739
usuyn <usuyn000@gmail.com>
2021-11-12 00:14:58 +0900

    Modify(main.js) : change request.url to url
    이미 선언되어 있는 url 변수를 사용하지 않아 수정했습니다.

--- NodeJS-practice/main.js
+++ NodeJS-practice/main.js
@@ -2,10 +2,10 @@ var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
   var url = request.url;
-  if(request.url == '/'){
+  if(url == '/'){
     url = '/index.html';
   }
-  if(request.url == '/favicon.ico'){
+  if(url == '/favicon.ico'){
     response.writeHead(404);
     response.end();
     return;
