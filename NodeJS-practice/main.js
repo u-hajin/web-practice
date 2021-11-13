e986d70c6c3d015397128f162773ff808f65258c
usuyn <usuyn000@gmail.com>
2021-11-14 02:51:54 +0900

    Refactor : new variable 'title' for queryData.id
    title, h2 태그에 사용되는 queryData.id를 직관적으로 표현하기 위해 새로운 변수 title을 생성했습니다.

--- NodeJS-practice/main.js
+++ NodeJS-practice/main.js
@@ -5,6 +5,7 @@ let url = require('url');
 let app = http.createServer(function(request,response){
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
+  let title = queryData.id;
   //console.log(_url);
   //console.log(queryData);
   //console.log(queryData.id);
@@ -23,7 +24,7 @@ let app = http.createServer(function(request,response){
   <!doctype html>
   <html>
   <head>
-    <title>WEB1 - ${queryData.id}</title>
+    <title>WEB1 - ${title}</title>
     <meta charset="utf-8">
   </head>
   <body>
@@ -33,7 +34,7 @@ let app = http.createServer(function(request,response){
       <li><a href="/?id=CSS">CSS</a></li>
       <li><a href="/?id=JavaScript">JavaScript</a></li>
     </ol>
-    <h2>${queryData.id}</h2>
+    <h2>${title}</h2>
     <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5   speicification">Hypertext Markup Language (HTML)</a> is the standard markup   language for <strong>creating <u>web</u> pages</strong> and web applications.  Web browsers receive HTML documents from a web server or from local storage   and render them into multimedia web pages. HTML describes the structure of a   web page semantically and originally included cues for the appearance of the   document.
     <img src="coding.jpg" width="100%">
     </p><p style="margin-top:45px;">HTML elements are the building blocks of   HTML pages. With HTML constructs, images and other objects, such as   interactive forms, may be embedded into the rendered page. It provides a   means to create structured documents by denoting structural semantics for   text such as headings, paragraphs, lists, links, quotes and other items.   HTML elements are delineated by tags, written using angle brackets.
