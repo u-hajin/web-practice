6c3b930f5fae405929bf7421d08544cd12934253
usuyn <usuyn000@gmail.com>
2021-11-20 23:47:16 +0900

    Add : Practice of NodeJS_19
    존재하지 않는 주소로 접속했을 때 오류 메시지를 전송하는 방법을 배웠습니다.
    사용자가 존재하지 않는 주소로 접속을 시도하면 Not found를 뜨게 했습니다.

--- NodeJS-practice/main.js
+++ NodeJS-practice/main.js
@@ -2,44 +2,45 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
-let app = http.createServer(function(request,response){
+let app = http.createServer(function (request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
+  let pathname = url.parse(_url, true).pathname;
   let title = queryData.id;
   //console.log(_url);
   //console.log(queryData);
   //console.log(queryData.id);
-  if(_url == '/'){
-    title = 'Welcome';
-  }
-  if(_url == '/favicon.ico'){
+
+  //console.log(url.parse(_url, true).pathname);
+  if (pathname === '/') {
+    fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+      let template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else {
     response.writeHead(404);
-    response.end();
-    return;
+    response.end('Not found');
   }
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, 'utf8', function(err, description){
-    let template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
-    response.end(template);
-  });
+
   //console.log(__dirname + _url);
   //response.end(fs.readFileSync(__dirname + _url));
 
